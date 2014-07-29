d4fb77b5bfba082f34462b6742101382e0bf169e
Arnd R. Strube <a.strube@wind-soft.de>
2014-07-29 12:10:27 +0200

    Put the real number of tests

--- tests/AllTestsCount1.cpp
+++ tests/AllTestsCount1.cpp
@@ -7,6 +7,14 @@ TEST_GROUP(AllTestsCount1)
 };
 TEST_ORDERED(AllTestsCount1, CountIsCorrect, 0xFFFFu)
 {
-    unsigned expected_number_of_tests = 376;
+    unsigned expected_number_of_tests = 132;
+#if CPPUTEST_USE_STD_CPP_LIB
+#else
+    expected_number_of_tests--; // TEST(MemoryLeakWarningGlobalDetectorTest, turnOffNewOverloadsNoThrowCausesNoAdditionalLeaks)
+    expected_number_of_tests--; // TEST(BasicBehavior, bothMallocAndFreeAreOverloaded)
+#endif
+#ifndef CPPUTEST_USE_MALLOC_MACROS
+    expected_number_of_tests--; // TEST(MemoryLeakOverridesToBeUsedInProductionCode, MallocOverrideIsUsed)
+#endif
     LONGS_EQUAL(expected_number_of_tests, TestRegistry::getCurrentRegistry()->countTests());
 }
--- tests/AllTestsCount2.cpp
+++ tests/AllTestsCount2.cpp
@@ -7,6 +7,14 @@ TEST_GROUP(AllTestsCount2)
 };
 TEST_ORDERED(AllTestsCount2, CountIsCorrect, 0xFFFFu)
 {
-    unsigned expected_number_of_tests = 179;
+    unsigned expected_number_of_tests = 237;
+#if CPPUTEST_USE_STD_CPP_LIB
+#else
+    expected_number_of_tests--; // TEST(UtestShell, DestructorIsCalledForLocalObjectsWhenTheTestFails)
+    expected_number_of_tests--; // TEST(UnitTestMacros, FailureWithCHECK_THROWS_whenDoesntThrow)
+    expected_number_of_tests--; // TEST(UnitTestMacros, SuccessWithCHECK_THROWS)
+    expected_number_of_tests--; // TEST(UnitTestMacros, FailureWithCHECK_THROWS_whenWrongThrow)
+    expected_number_of_tests--; // TEST(UnitTestMacros, MultipleCHECK_THROWS_inOneScope)
+#endif
     LONGS_EQUAL(expected_number_of_tests, TestRegistry::getCurrentRegistry()->countTests());
 }
