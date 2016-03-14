afa5c1334bf38a2d74fdebd4815832d03843eddd
Arnd R. Strube <a.strube@wind-soft.de>
2016-03-14 10:18:53 +0100

    ....

--- src/Platforms/Gcc/UtestPlatform.cpp
+++ src/Platforms/Gcc/UtestPlatform.cpp
@@ -232,10 +232,10 @@ void (*PlatformSpecificFree)(void* memory) = free;
 void* (*PlatformSpecificMemCpy)(void*, const void*, size_t) = memcpy;
 void* (*PlatformSpecificMemset)(void*, int, size_t) = memset;
 
-/* GCC 4.9.x introduces -Wfloat-conversion, which causes a warning / error
- * in its own (macro) implementation of isnan() and isinf()
+/* GCC 4.9.x introduces -Wfloat-conversion, which causes a warning / error in its own (macro)
+ * implementation of isnan() and isinf(). And Clang has __GNUC__ but not GNUC_MINOR__.
  */
-#if defined(__GNUC__) && !defined(__clang__)
+#if defined(__GNUC__) && !defined(GNUC_MINOR__)
  #if __GNUC__ >= 5 || ( __GNUC__ == 4 && GNUC_MINOR__ > 8 )
   #pragma GCC diagnostic ignored "-Wfloat-conversion"
  #endif
