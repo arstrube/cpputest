6db29f2ad1edeec31802f8389b401ddd190758ac
Arnd R. Strube <a.strube@wind-soft.de>
2016-03-14 09:07:04 +0100

    isnan() fix

--- src/Platforms/Gcc/UtestPlatform.cpp
+++ src/Platforms/Gcc/UtestPlatform.cpp
@@ -235,7 +235,7 @@ void* (*PlatformSpecificMemset)(void*, int, size_t) = memset;
 /* MinGw GCC 5.3.0 isnan / isinf macros are not working properly, causing a conversion
  * warning / error
  */
-#if defined(__GNUC__) && __GNUC__ >= 5
+#if defined(__GNUC__) && ( __GNUC__ >= 5 || ( __GNUC__ == 4 && GNUC_MINOR__ > 8 ) )
 #pragma GCC diagnostic ignored "-Wfloat-conversion"
 #endif
 
